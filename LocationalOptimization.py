f1f690c90fd43f5e8f8412bcb6415d72b40a8102
Joel Abraham <joelpabraham@gmail.com>
2017-08-02 01:13:17 -0500

    Update LocationalOptimization.py

--- LocationalOptimization.py
+++ LocationalOptimization.py
@@ -1,8 +1,8 @@
-import scipy as sp
+import scipy.optimize
 
 global M, N, k, m, Z, length;
-m = 371650273390117;
-M = 5; N = 5; k = 5; Z = 5;
+m = 31.371650273390117;
+M = 5; N = 5; k = 5; Z = 5; # adjustible
 length = M*N
 
 def cost(LEDconfiguration):
@@ -13,18 +13,30 @@ def cost(LEDconfiguration):
 	for target in range(length):
 		point = [target%M, target//M]
 		for LED in LEDconfiguration:
-			LEDcoord = [LED%M,LED//M]; print(4);
-			netirradiance += (Z**(m+1))/((((point[0]-LEDcoord[0])**2) + ((point[1]-LEDcoord[1])**2) + Z**2)**((m+3)/2));;
+			LEDcoord = [LED%M,LED//M];
+			netirradiance += (Z**(m+1))/\
+                        ((((point[0]-LEDcoord[0])**2) + ((point[1]-LEDcoord[1])**2) + Z**2)**((m+3)/2));
 	avgirradiance = netirradiance/length;
 	for target in range(length):
 		point = [target%M, target//M]
 		pointirradiance = 0
 		for LED in LEDconfiguration:
 			LEDcoord = [LED%M,LED//M]
-			pointirradiance += (Z**(m+1))/((((point[0]-LEDcoord[0])**2) + ((point[1]-LEDcoord[1])**2) + (Z**2))**((m+3)/2))
+			pointirradiance += (Z**(m+1))/\
+                        ((((point[0]-LEDcoord[0])**2) + ((point[1]-LEDcoord[1])**2) + (Z**2))**((m+3)/2))
 		neterrorsquared+=((pointirradiance - avgirradiance)**2)
 	sigma = (neterrorsquared/length)**(0.5)
 	CVRMSE = sigma/avgirradiance
 	return CVRMSE
 
+guess = [1]*k;
+
+constraints = [(0, length)]*k
+
+# scipy SLSQP optimization heuristic
+minimum=scipy.optimize.minimize(cost, [1, 2, 3, 4, 5], method='SLSQP', bounds=constraints);
+
+print(minimum)
+
+# globally optimal solution based on complete space search
 print(cost([2, 5, 9, 16, 18]))
